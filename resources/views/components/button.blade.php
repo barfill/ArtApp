b8fe839e1ee9cb903f86eb536e955685fb892589
bartekF <bartosz_filipczak@op.pl>
2025-05-31 12:45:31 +0200

    Button operator comment added

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,7 +1,7 @@
 <button
-    type="{{ $type ?? 'button'}}"
+    type="{{ $type ?? 'button'}}" {{-- Tutaj sprawdzma tylko czy type jest null --}}
     class="btn {{ $class ?? '' }}"
     action="{{ $action ?? '' }}"
     {{ $disabled ? 'disabled' : '' }}>
-    {{ $slot ?: $label }}
+    {{ $slot ?: $label }} {{-- Tutaj sprawdzma tylko czy $slot jest truthy/falsy czyli jeżeli $slot będzie truthy to wybierzemy te wartość a jak falsy to $label--}}
 </button>
